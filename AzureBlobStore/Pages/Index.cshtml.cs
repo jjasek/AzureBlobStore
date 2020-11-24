2bb053c0ef54f4e24039a7ee8e1086dde09eb6d8
anigh3 <anigh@vp.pl>
2020-11-24 21:15:54 +0100

    Dodaj pliki projektów.

---
+++ AzureBlobStore/Pages/Index.cshtml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Threading.Tasks;
+using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace AzureBlobStore.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        private BlobServiceClient _bsc;
+        private BlobContainerClient _container;
+        public Uri uri;
+        public List<string> pictures;
+
+        [BindProperty]
+        public IFormFile[] UploadedFile { get; set; }
+
+        public IndexModel(ILogger<IndexModel> logger, BlobServiceClient bsc)
+        {
+            _logger = logger;
+            _bsc = bsc;
+            _container = _bsc.GetBlobContainerClient("pictures");
+            _container.CreateIfNotExistsAsync();
+            uri = _bsc.Uri;
+            pictures = getPictureNames();
+        }
+
+        public void OnGet()
+        {
+        }
+
+        public async Task OnPostAsync()
+        {
+            var bh = new BlobHttpHeaders();
+            bh.ContentType = "image/jpg";
+
+            foreach (var file in UploadedFile)
+            {
+                var blobClient = _container.GetBlobClient($"{Guid.NewGuid()}.jpg");
+                await blobClient.UploadAsync(file.OpenReadStream());
+                await blobClient.SetHttpHeadersAsync(bh);
+            }
+            pictures = getPictureNames();
+        }
+
+        private List<string> getPictureNames()
+        {
+            return _container.GetBlobs().Select(p => p.Name).ToList();
+        }
+    }
+}
---
+++ AzureBlobStore/Services/AzureBlobServices.cs
@@ -0,0 +1,43 @@
+﻿using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AzureBlobStore.Services
+{
+    public class AzureBlobServices
+    {
+        public async Task UploadImage(List<string> imagePaths)
+        {
+            BlobServiceClient bsc = new BlobServiceClient("DefaultEndpointsProtocol=https;AccountName=tgrstorage;AccountKey=07YNLuRL4Sw2cGHLLnXA1hVuPDOqyg63PJBhb/vqJJQV/q4X5eZZV+KaEjhCXRk/iXw6tCztiryx0lp8qmPfHg==;EndpointSuffix=core.windows.net");
+            var container = bsc.GetBlobContainerClient("pictures");
+            await container.CreateIfNotExistsAsync();
+
+            var bh = new BlobHttpHeaders();
+            bh.ContentType = "image/jpg";
+
+            var blobClient = container.GetBlobClient($"{Guid.NewGuid()}.jpg");
+            foreach(var i in imagePaths)
+            {
+                await blobClient.UploadAsync(i);
+            }
+            await blobClient.SetHttpHeadersAsync(bh);
+        }
+
+        public List<Stream> DownloadImage()
+        {
+            BlobServiceClient bsc = new BlobServiceClient("DefaultEndpointsProtocol=https;AccountName=tgrstorage;AccountKey=07YNLuRL4Sw2cGHLLnXA1hVuPDOqyg63PJBhb/vqJJQV/q4X5eZZV+KaEjhCXRk/iXw6tCztiryx0lp8qmPfHg==;EndpointSuffix=core.windows.net");
+            var container = bsc.GetBlobContainerClient("pictures");
+            List<Stream> blobDownloadInfos = new List<Stream>();
+            container.GetBlobs().Select(p => p.Name).ToList().ForEach(async p =>
+            {
+                BlobDownloadInfo download = await container.GetBlobClient(p).DownloadAsync();
+                blobDownloadInfos.Add(download.Content);
+            });
+            return blobDownloadInfos;
+        }
+    }
+}
